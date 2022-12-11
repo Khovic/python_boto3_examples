5ec6812f395ada291c05ed2d30a976d5bc935fc9
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-11 14:51:10 +0200

    created new EX3

---
+++ 3ex_automate_run_monitor.py
@@ -0,0 +1,22 @@
+import boto3
+import time
+ec2_resource = boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
+
+instances = ec2_resource.create_instances(
+    ImageId="ami-076309742d466ad69",
+    MinCount=1,
+    MaxCount=1,
+    InstanceType="t2.micro",
+    KeyName="poopy-oracle",
+
+    NetworkInterfaces=[
+            {
+            'AssociatePublicIpAddress': True,
+            'DeviceIndex': 0,
+            'SubnetId' : 'subnet-073db04a00010f052'
+            }
+    ]
+)
+
+print(instances)
