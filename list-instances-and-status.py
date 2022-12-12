ec4cba28a96dccefbecf323c7b0f3cd643594c06
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-12 12:47:26 +0200

    created list-instances-and-status.py

---
+++ list-instances-and-status.py
@@ -0,0 +1,18 @@
+import boto3
+
+ec2_resource = boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
+
+def check_status():
+    print()
+
+described_instance = ec2_client.describe_instances()
+reservations = described_instance["Reservations"]
+
+for reservation in reservation:
+    for instance in reservation["Instances"]:
+        print(instance["InstanceId"])
+        print(instance["State"]["Name"])
+
+
+check_status()
