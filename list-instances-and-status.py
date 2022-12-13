01a3551e0f247519bada2ff94f5dc038320b2d48
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-13 11:13:49 +0200

    added volumes listing

--- list-instances-and-status.py
+++ list-instances-and-status.py
@@ -9,9 +9,24 @@ reservations = described_instance["Reservations"]
 described_instance_statutes = ec2_client.describe_instance_status(
 )
 
+described_volume = client.describe_volumes(
+   VolumeIds=[
+        'string',
+    ],
+)
+
 def check_status():
     for status in described_instance_statutes['InstanceStatuses']:
         print(f'instance {status["InstanceId"]} is {status["InstanceState"]["Name"]} and system status is {status["SystemStatus"]["Status"]}')
+
+        described_volumes = client.describe_volumes(
+        Filters=[{'Name': 'attachment.instance-id', 'Values': [status["InstanceId"]]}]
+        )
+
+        for volume in described_volumes['Volumes']:
+            print("attached volumes: ")
+            for attachment in volume['Attachments']:
+                print(f"{attachment['VolumeId']}")
 
 
 check_status()
