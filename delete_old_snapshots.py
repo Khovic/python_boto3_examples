abfe2d8c31477c355e2c764bcb27e2cb6b2b8a88
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-13 15:46:39 +0200

    added delete_old_snapshots

--- delete_old_snapshots.py
+++ delete_old_snapshots.py
@@ -15,10 +15,12 @@ def delete_snapshots(tag_name, tag_value):
         ],
     )
 
+    #sorts the described_snapshots in a descending order.
     sorted_snapshots = sorted(described_snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
 
+    #loops through sorted_snapshots skipping first 2 items and deletes old snapshots
     for snapshot in sorted_snapshots[2:]:
         #ec2_client.delete_snapshot(SnapshotId=snapshot['SnapshotId'],)
-        print(f"snapshot {snapshot['StartTime']} has been deleted")
+        print(f"snapshot {snapshot['SnapshotId']} dated {snapshot['StartTime']} has been deleted")
 
 delete_snapshots('created-by', 'python-script')
