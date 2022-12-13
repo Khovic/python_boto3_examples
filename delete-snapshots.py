3aa57c3abf433f0042db23dd2816ae2759f0600d
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-13 12:07:45 +0200

    added - delete_snapshots

--- delete-snapshots.py
+++ delete-snapshots.py
@@ -3,7 +3,16 @@ import boto3
 ec2_resource = boto3.resource('ec2')
 ec2_client = boto3.client('ec2')
 
-described_snapshots = ec2_client.describe_snapshots()
+described_snapshots = ec2_client.describe_snapshots(
+        Filters=[
+        {
+            'Name': 'Tag:created-by',
+            'Values': [
+                'python-script',
+            ]
+        },
+    ],
+)
 
 for snapshot in described_snapshots['Snapshots']:
     print(snapshot['SnapshotId'])
