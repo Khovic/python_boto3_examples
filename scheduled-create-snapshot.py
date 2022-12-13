d850f1223f54943da0cf1b0e1f8db4567a5d530c
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-13 14:22:05 +0200

    added scheduled-create-snapshot.py

--- scheduled-create-snapshot.py
+++ scheduled-create-snapshot.py
@@ -2,11 +2,7 @@ import schedule
 import create_snapshots
 import time
 
-#schedule.every(5).minutes.do(create_snapshots.create_snapshots('created-by', 'python-script'))
+schedule.every(5).minutes.do(create_snapshots.create_snapshots('created-by', 'python-script'))
 
-def print_poo():
-    print('poopoo')
-
-schedule.every(1).minutes.do(print_poo)
 while True:
     schedule.run_pending()
