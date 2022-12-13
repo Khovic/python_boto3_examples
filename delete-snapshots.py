371e642c83e491b413a05a7a504fcc76964939be
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-13 13:48:05 +0200

    added tags to create_snapshot

--- delete-snapshots.py
+++ delete-snapshots.py
@@ -2,18 +2,20 @@ import boto3
 
 ec2_resource = boto3.resource('ec2')
 ec2_client = boto3.client('ec2')
+def delete_snapshots(tag_name, tag_value):
+    described_snapshots = ec2_client.describe_snapshots(
+            Filters=[
+            {
+                'Name': 'tag:'+tag_name,
+                'Values': [
+                    tag_value,
+                ]
+            },
+        ],
+    )
 
-described_snapshots = ec2_client.describe_snapshots(
-        Filters=[
-        {
-            'Name': 'tag:created-by',
-            'Values': [
-                'python-script',
-            ]
-        },
-    ],
-)
+    for snapshot in described_snapshots['Snapshots']:
+        ec2_client.delete_snapshot(SnapshotId=snapshot['SnapshotId'],)
+        print(f"snapshot {snapshot['SnapshotId']} has been deleted")
 
-for snapshot in described_snapshots['Snapshots']:
-    ec2_client.delete_snapshot(SnapshotId=snapshot['SnapshotId'],)
-    print(f"snapshot {snapshot['SnapshotId']} has been deleted")
+delete_snapshots('created-by', 'python-script')
