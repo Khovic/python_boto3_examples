9b91a5244e384eb21fc2611a5668d194b2e81cb3
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-14 10:30:53 +0200

    added email messager

--- website_monitor.py
+++ website_monitor.py
@@ -11,20 +11,25 @@ EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 def send_email ():
     print("Possible error with app, sending email notification")
 
-    if response.status_code == 200:
-        print("Success")
-    else: 
-        print("Possible error with app, sending email notification")
-        send_email()
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.starttls()
         smtp.ehlo()
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.sendmail(EMAIL_PASSWORD, EMAIL_PASSWORD, email_message)
 
+
+    if response.status_code == 200:
+        print("Success")
+    else: 
+        print("Possible error with app, sending email notification")
+        send_email()
+
 try:
     response = requests.get('http://129.159.151.65:8080')
+    if response.status_code == 200:
+        print("Success")
 except:
+    print("Possible error with app, sending email notification")
     send_email()
 
 
