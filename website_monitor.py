0eec69e06bf85620858c5b374d007ddfce71cdc1
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-14 09:51:07 +0200

    added website_monitor

--- website_monitor.py
+++ website_monitor.py
@@ -3,7 +3,7 @@ import requests
 website_address = "129.159.151.65:8080"
 response = requests.get('http://129.159.151.65:8080')
 
-print(response)
+print(response.status_code)
 
 if response.ok == "200":
     print("Success")
