23475885721b58d5f75781320a003fe142a6e299
Khovic <khovic@email.com>
2022-12-17 15:38:48 +0200

    3ex_.. now restart nginx container after 5 consecutive failures

---
+++ 4ex_ecr.py
@@ -0,0 +1,21 @@
+import boto3
+from operator import itemgetter
+
+ecr_client = boto3.client('ecr')
+described_repos = ecr_client.describe_repositories()
+
+#name of the repo we want
+specified_repo_name = 'java-mysql-app'
+
+for repo in described_repos['repositories']:
+    print(repo['repositoryName'])
+    if repo['repositoryName'] == specified_repo_name:
+
+        described_images = ecr_client.describe_images(repositoryName=repo['repositoryName'],)
+        sorted_images = sorted(described_images['imageDetails'], key=itemgetter('imagePushedAt'), reverse=True)
+
+        for image in sorted_images:
+            try:
+               print(f'image {image["repositoryName"]}:{image["imageTags"]} pushed at {image["imagePushedAt"]}')
+            except:
+                print(f'image {image["repositoryName"]} has a tag error')
